cd64805ef4f3f376449906b04f9403db2f00591e
DESKTOP-TKB7BH6\Asus <sayemseer@gmail.com>
2022-11-16 21:58:42 +0600

    Create and Edit Actor Form

--- react-movie/src/actors/CreateActor.tsx
+++ react-movie/src/actors/CreateActor.tsx
@@ -1,7 +1,13 @@
+import ActorForm from "./ActorForm";
+
 export default function CreateActor() {
-    return (
-        <div>
-            <h3>Create Actor</h3>
-        </div>
-    );
+  return (
+    <div>
+      <h3>Create Actor</h3>
+      <ActorForm
+        model={{ name: "", dateOfBirth: undefined }}
+        onSubmit={(values) => console.log(values)}
+      />
+    </div>
+  );
 }
