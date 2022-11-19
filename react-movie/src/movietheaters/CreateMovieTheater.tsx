4ea92ad43c8f623ce4b2ae7db9fbb62131ad5198
DESKTOP-TKB7BH6\Asus <sayemseer@gmail.com>
2022-11-19 09:52:55 +0600

    Added cread, edit movieTheater

--- react-movie/src/movietheaters/CreateMovieTheater.tsx
+++ react-movie/src/movietheaters/CreateMovieTheater.tsx
@@ -1,7 +1,13 @@
+import MovieTheterForm from "./MovieTheaterForm";
+
 export default function CreateMovieTheater() {
-    return (
-        <div>
-            <h3>Create Movie Theater</h3>
-        </div>
-    );
+  return (
+    <div>
+      <h3>Create Movie Theater</h3>
+      <MovieTheterForm
+        model={{ name: "" }}
+        onSubmit={(values) => console.log(values)}
+      />
+    </div>
+  );
 }
--- react-movie/src/movietheaters/EditMovieTheater.tsx
+++ react-movie/src/movietheaters/EditMovieTheater.tsx
@@ -1,7 +1,13 @@
+import MovieTheterForm from "./MovieTheaterForm";
+
 export default function EditMovieTheater() {
-    return (
-        <div>
-            <h3>Edit Movie Theater</h3>
-        </div>
-    );
+  return (
+    <div>
+      <h3>Edit Movie Theater</h3>
+      <MovieTheterForm
+        model={{ name: "Sambil" }}
+        onSubmit={(values) => console.log(values)}
+      />
+    </div>
+  );
 }
