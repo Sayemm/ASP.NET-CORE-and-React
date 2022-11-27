ec79a19d69f50bd6ca5b31b8a3823d18d3c23f49
DESKTOP-TKB7BH6\Asus <sayemseer@gmail.com>
2022-11-27 15:08:00 +0600

    Web Api

---
+++ MoviesAPI/MoviesAPI/Program.cs
@@ -0,0 +1,31 @@
+using MoviesAPI.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//My Services
+//Wheever we ask IRepository service, the DI system serves an InMemoryRepository instance
+builder.Services.AddSingleton<IRepository, InMemoryRepository>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
