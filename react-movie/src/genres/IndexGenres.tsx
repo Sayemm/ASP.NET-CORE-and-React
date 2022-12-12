e3acf73d9e5fd0bd80aa3632f00b85f65b5e9f72
DESKTOP-TKB7BH6\Asus <sayemseer@gmail.com>
2022-12-12 22:54:27 +0600

    axios

--- react-movie/src/genres/IndexGenres.tsx
+++ react-movie/src/genres/IndexGenres.tsx
@@ -1,12 +1,28 @@
+import axios, { AxiosResponse } from "axios";
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
+import { urlGenres } from "../endpoints";
+import { genreDTO } from "./genres.model";
 
 export default function IndexGenres() {
+    useEffect(() => {
+        axios
+            .get(urlGenres)
+            .then((response: AxiosResponse<genreDTO[]>) => {
+                console.log(response.data);
+            });
+    }, []);
+
     return (
         <div>
             <h3>Genres</h3>
-            <Link className="btn btn-primary" to="/genres/create">Create Genre</Link>
-            <br /> <br />
-            <Link className="btn btn-primary" to="/genres/edit">Edit Genre</Link>
+            <Link className="btn btn-primary" to="/genres/create">
+                Create Genre
+            </Link>
+            {/* <br /> <br />
+            <Link className="btn btn-primary" to="/genres/edit">
+                Edit Genre
+            </Link> */}
         </div>
     );
 }
